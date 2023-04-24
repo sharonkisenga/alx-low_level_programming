1d495c3c37d62e4105f08c52e9b07226d5de8c0a
sharonkisenga <sharonglor69@gmail.com>
2023-04-24 02:51:57 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main Entry - point 
- * Description: 'print combination of two digit'
- * Return: always 0
+ * main - Entry point
+ * Description: 'print two digits'
+ * Return: Always 0
  */
 int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = '0'; n < '9'; n++)
 	{
-		putchar(n);
-	for (m = 48; m <= 57; n++)
-	
-		putchar(m);
-		if (n == 57, m == 57);
-	        {
-			continue;
-		}
-		putchar(',');
-		putchar(' ');
+	for (m = n + 1; m <= '9'; m++)
+	{
+	if (m != n)
+	{
+	putchar(n);
+	putchar(m);
+	if (n == '0' && m == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
