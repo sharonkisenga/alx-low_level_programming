66918c491ddc2f62e1ead350c266576eb59f3c43
sharonkisenga <sharonglor69@gmail.com>
2023-04-25 15:19:47 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- *_isalpha - shows 1 if the input is a
- * letter Another cases, shows 0
- *@c: The character in ASCII code
+ * _isalpha - shows 1 if the input is a 
+ * letter another cases, shows 0
+ * @c: the character in ASCII code
  * Return: 1 for letters. 0 for the rest.
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 98))
 	{
 		return (0);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
