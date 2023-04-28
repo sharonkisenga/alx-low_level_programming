10c9bd7b2c5cf5a43dab409afca11a09faced5b1
sharonkisenga <sharonglor69@gmail.com>
2023-04-28 03:03:44 -0700

    task8

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-	char x;
+	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-	_putchar(x);
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
- * print_square - prints a square, followed by a new line
- * @size: size of the square
+ * print_square - prints a squares according n number of times
+ * @size: the number of squares/number of times
+ * return: empty
  */
 void print_square(int size)
 {
+	int x, y;
+
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else 
 	{
-		int i, j;
-
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('0');
-			}
-			_putchar('\n');
-			}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
