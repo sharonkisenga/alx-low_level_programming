0db8985ba63fa3e3eb937192414efe4390000476
sharonkisenga <sharonglor69@gmail.com>
2023-04-27 17:02:07 -0700

    task11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,7 +11,7 @@ int main(void)
         long int max;
         long int i;
 
-        n = 612852475143
+        n = 612852475143;
         max = -1;
 
         while (n % 2 == 0)
@@ -19,7 +19,7 @@ int main(void)
                 max = 2;
                 n /=2;
         }
-        for (i = 3; i <= sqrt(n): i = i +2)
+        for (i = 3; i <= sqrt(n); i = i +2)
         {
                 while (n % i == 0)
                 {
