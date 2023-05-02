0a82ea6d6ac63b08eb283e76a7118be7148e54b6
sharonkisenga <sharonglor69@gmail.com>
2023-05-02 12:23:12 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts_half - prints half of a string 
- * @str: the string to print 
+ * puts_half - prints half of a string
+ * @str: the string to print
  * return: void
  */
 void puts_half(char *str)
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	k = (j - 1) / 2;
 	k += 1;
 	}
-	else 
+	else
 	{
 	k = j / 2;
 	}
