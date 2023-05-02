1957af5ae32ca54e6b128a1d2b88a5f913027957
sharonkisenga <sharonglor69@gmail.com>
2023-05-02 12:19:51 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * rev_string - reverses a string 
+ * rev_string - reverses a string
  * @s: the string to be modified
  * return: void
  */
@@ -11,10 +11,10 @@ void rev_string(char *s)
 
 	while (s[index++])
 	len++;
-	for (index = len -1; index >= len / 2; index--)
+	for (index = len - 1; index >= len / 2; index--)
 	{
 	tmp = s[index];
-	s[index] = s[len - index -1];
-	s[len - index -1] = tmp;
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
