e8f57e5b2ffae52e668a8b9d9854edaa14d75a81
sharonkisenga <sharonglor69@gmail.com>
2023-05-04 11:23:36 -0700

    task8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to atring params
+ * return: *s
+ */
+char *rot13(char *s)
+{
+	int k, l;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMNnopqrstuvwxyzabcdefghijklm";
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+	for (l = 0; l < 52; l++)
+	{
+	if (s[k] == data1[l])
+	{
+		s[k] = datarot[l];
+		break;
+	}
+	}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,8 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *leet(char *);
+char *rot13(char *);
+
 
 
 
