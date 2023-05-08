adbff1a819732e0f963e8ab74cf087b5aa8cb7e0
sharonkisenga <sharonglor69@gmail.com>
2023-05-08 02:23:31 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * return: always 0 (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+		dest[j] = src[j];
+	return (dest);
+}
