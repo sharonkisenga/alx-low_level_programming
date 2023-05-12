69230d3d692560f51773d778f3a6f9571efc89ec
sharonkisenga <sharonglor69@gmail.com>
2023-05-12 06:47:36 -0700

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - print all argument it receives
+ * main - print all argument it receives all argument should be printed includind the fiest one
  * @argc: argument count
  * @argv: argument vector
  * return: always 0
