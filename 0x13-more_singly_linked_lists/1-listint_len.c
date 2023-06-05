695bc3353aa46c08156e90207ccbea2c05cef2e5
sharonkisenga <sharonglor69@gmail.com>
2023-06-05 03:34:03 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns the nument of elements in listint_t
+ * @h: linked listint_t 
+ * return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
