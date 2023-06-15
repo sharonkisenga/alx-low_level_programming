596a738042ec250686e24bd932072f562c5726c6
sharonkisenga <sharonglor69@gmail.com>
2023-06-15 04:22:25 -0700

    100-elf_header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int __attribute__((__unused__)) argc, char *argv[])
 int _putchar(char i);
+void close_elf(int elf);
 
 #endif
